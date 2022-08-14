3d2991e604af195a1d01a0e682c3b4bd1ee12354
Miguel Blanque <miguelblanque@gmail.com>
2022-08-14 07:48:21 +0200

    Creacion de Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import AppLetras from "./components/AppLetras"
+
+function App() {
+
+  return (
+    <AppLetras/>
+  )
+}
+
+export default App
---
+++ src/components/AppLetras.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Formulario from './Formulario'
+
+const AppLetras = () => {
+  return (
+   <>
+        <header>Búsqueda de Letras de Canciones</header>
+
+        <Formulario/>
+        
+        <main>
+          
+        </main>
+   </>
+  )
+}
+
+export default AppLetras
